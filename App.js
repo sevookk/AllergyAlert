561d78180d67de5fecdbc2ade8ba5703721e7fd7
Daniel Hacobian <danielhacobian@Daniels-MacBook-Air.local>
2023-10-04 11:20:55 -0700

    upload

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, Text, View, TouchableOpacity, ScrollView, Alert, Image } from 'react-native';
 
 export default function App() {
+  const [selectedAllergens, setSelectedAllergens] = useState([]);
+  const allergens = ['Peanuts', 'Milk', 'Gluten', 'Soy', 'Fish', 'Eggs', 'Shellfish', 'Tree Nuts'];
+
+  const handleAllergenSelect = (allergen) => {
+    if (selectedAllergens.includes(allergen)) {
+      setSelectedAllergens(selectedAllergens.filter(item => item !== allergen));
+    } else {
+      setSelectedAllergens([...selectedAllergens, allergen]);
+    }
+  };
+
+  const handleLogin = () => {
+    // Handle the login logic here, such as saving the selectedAllergens to a database
+    Alert.alert('Allergy Information Saved', `You are allergic to: ${selectedAllergens.join(', ')}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Image
+          source={require('./images/ALLERGY.png')}  // Replace with your image's relative path
+          style={styles.logo}
+        />
+      </View>
+      <Text style={styles.title}>Select Your Allergens</Text>
+      <ScrollView style={styles.allergenList}>
+        {allergens.map((allergen, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.allergenButton,
+              selectedAllergens.includes(allergen) ? styles.selected : {}
+            ]}
+            onPress={() => handleAllergenSelect(allergen)}
+          >
+            <Text style={styles.buttonText}>{allergen}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +54,47 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    width: '100%',
+    padding: 10,
+    paddingTop: 40,  // Added top padding
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  allergenList: {
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  allergenButton: {
+    backgroundColor: '#f0f0f0',
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+  selected: {
+    backgroundColor: '#e65054',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#000',
+  },
+  loginButton: {
+    backgroundColor: '#f0f0f0',
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
   },
 });
